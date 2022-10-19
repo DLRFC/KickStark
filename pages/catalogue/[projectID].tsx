41f43d1236227527b3cd7fc5e86f2d0ed5367c83
Colleen Rose <cjrose59@gmail.com>
2022-10-18 19:04:24 -0700

    page building notes

--- pages/catalogue/[projectID].tsx
+++ pages/catalogue/[projectID].tsx
@@ -1,5 +1,17 @@
 import { NextPage } from "next";
+import { text } from "node:stream/consumers";
 import Header from "../../components/header";
+import InvestorDashboard from "../investors/[investorID]";
+
+// This page is initialized when builders sbmit the first form with profile info,
+// then they arrive here to their new page to fill out roadmap building form.
+// when roadmap is submitted the page displays profile card, roadmap component and progess/status component
+// All contract interactions will happen from this page. 
+// Will include buttons to 
+// 1)deposit funds (always available),
+// 2)builders submit checkpoint requirements/claim funds (diasbled unless wallet is project owner),
+// 3)collect refunds (disabled unless wallet is investor).
+// Each button should pop up a modal to set params of the transaction.
 
 const ProjectProfile: NextPage = () => {
     return (
--- pages/investors/[investorID].tsx
+++ pages/investors/[investorID].tsx
@@ -3,7 +3,10 @@ import { NextPage } from "next";
 import Header from "../../components/header";
 
 // right now this page is only accessible by typing into browser url
-// should give the option to go here when user connects wallet and has already funded something
+// need to give the option to go here when user connects wallet and has already funded something.
+// Investor's dashboard will show individual wallet's portfolio of investments, 
+// with each project's summary, current status and link to project's dedicated page.
+// All interactions will happen from the project page.
 
 const InvestorDashboard: NextPage = () => {
     return (
