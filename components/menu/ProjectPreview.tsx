d296b15e78200a1f54011f9386257ab54ba5501b
Colleen Rose <cjrose59@gmail.com>
2022-10-21 13:56:40 -0700

    update menu item layout

--- components/menu/ProjectPreview.tsx
+++ components/menu/ProjectPreview.tsx
@@ -5,8 +5,10 @@ import React, { FC } from "react";
 // status inside the 'Deliverable' heading of front page menu
 const ProjectPreview: FC = () => {
     return (
-        <div className="w-[900px] h-[200px] flex justify-center place-items-center mb-10 ml-[15%] bg-brand-green text-brand-darkest">
-            <div>This will display a sample project Card, Roadmap, and Progress Report.</div>
+        <div className="flex item-center ml-[10%]">
+            <div className="h-[300px] w-[200px] mx-4 bg-brand-gray opacity-[90%] rounded-lg">Sample Card</div>
+            <div className="h-[300px] w-[400px] mx-4 bg-brand-gray opacity-[90%] rounded-lg">Sample Roadmap</div>
+            <div className="h-[300px] w-[275px] mx-4 bg-brand-gray opacity-[90%] rounded-lg">Sample Progess</div>
         </div>
     )
 }
