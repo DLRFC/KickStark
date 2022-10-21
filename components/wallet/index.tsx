23873bdde7b5e1103f6034af64116dee60479807
Rich Warner <richwarneriii@gmail.com>
2022-10-21 09:56:59 -0600

    Added additional contract calls

--- components/wallet/index.tsx
+++ components/wallet/index.tsx
@@ -1,11 +1,18 @@
 import { FC, useState } from "react"
 import { connect, getStarknet } from "get-starknet"
 import { Abi, Contract, Signature } from "starknet"
-import KickStarkAbi from "../../abis/KickStarkAbi.json"
+
+import kickStarkAbi from "../../abis/kickStarkAbi.json"
 
 const Wallet: FC = () => {
     const [isConnected, setIsConnected] = useState<boolean>(false)
     const [userAddress, setUserAddress] = useState<any>()
+
+    const network = "goerli-alpha"
+    const contractAddress =
+        "0x004992699a49a8ef380462c60228a9cebbcd5a4dcb9bcc58bc8d5981296ec8c5"
+    const validatorAddr =
+        "0x06dbdcf5d22ac87eb14d994a47e38aa5aa2eee2c6e3c0e391f04e14b8106473b"
 
     const connectWallet = async () => {
         console.log("connecting...")
@@ -26,43 +33,66 @@ const Wallet: FC = () => {
         }
     }
 
-    const contribute = async () => {
-        const network = "goerli-alpha"
+    const startProject = async () => {
         const wallet = getStarknet()
-        const validatorAddr =
-            "0x06dbdcf5d22ac87eb14d994a47e38aa5aa2eee2c6e3c0e391f04e14b8106473b"
         if (wallet.isConnected) {
-            const contractAddress =
-                network === "goerli-alpha"
-                    ? "0x004992699a49a8ef380462c60228a9cebbcd5a4dcb9bcc58bc8d5981296ec8c5"
-                    : network === "dev"
-                    ? "0x0398f8d51b907f824c210fa2ca0a01bec0b29a4bba0927d5667eaa6ef3899fe9" // need to update
-                    : undefined
-            if (!contractAddress) {
-                return undefined
-            }
-
             const kickStarkContract = new Contract(
-                KickStarkAbi as Abi,
+                kickStarkAbi as Abi,
                 contractAddress,
                 wallet.account
             )
+            return kickStarkContract.start_project(validatorAddr)
+        }
+    }
 
+    const contribute = async () => {
+        const wallet = getStarknet()
+        if (wallet.isConnected) {
+            const kickStarkContract = new Contract(
+                kickStarkAbi as Abi,
+                contractAddress,
+                wallet.account
+            )
             // kickStarkContract.start_project(validatorAddr)
-
             return kickStarkContract.contribute(1)
+        }
+    }
+
+    const stagePercentages = async () => {
+        const wallet = getStarknet()
+        if (wallet.isConnected) {
+            const kickStarkContract = new Contract(
+                kickStarkAbi as Abi,
+                contractAddress,
+                wallet.account
+            )
+            return kickStarkContract.stagePercentages(0)
         }
     }
 
     return (
         <>
             {isConnected && (
-                <button
-                    onClick={contribute}
-                    className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
-                >
-                    Deposit Funds
-                </button>
+                <>
+                    <button
+                        onClick={async () => alert(await stagePercentages())}
+                        className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
+                    >
+                        stagePercentages(0)
+                    </button>
+                    <button
+                        onClick={startProject}
+                        className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
+                    >
+                        start_project()
+                    </button>
+                    <button
+                        onClick={contribute}
+                        className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
+                    >
+                        contribute(1)
+                    </button>
+                </>
             )}
             <button
                 onClick={connectWallet}
