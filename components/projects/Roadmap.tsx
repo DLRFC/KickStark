bade972fda1d0626887de32459ad0bbe1b5d861d
Colleen Rose <cjrose59@gmail.com>
2022-10-23 15:39:25 -0700

    add current/selected phase conditions

--- components/projects/Roadmap.tsx
+++ components/projects/Roadmap.tsx
@@ -13,13 +13,11 @@ type RoadmapProps = {
     phaseDescriptions: string[][];
 }
 
-const Index = 0
-
 // this will create roadmap component from MapForm inputs
 const Roadmap: FC<RoadmapProps> = ({ phaseSummaries, phaseDescriptions}) => {
-    const [CurrentPhase, setCurrentPhase] = useState(0)
-    const [SelectedPhase, setSelectedPhase] = useState(0)
-    const handleSetSelectedPhase = (Index: number) => (SelectedPhase === Index ? setSelectedPhase(1) : setSelectedPhase(Index))
+    const [CurrentPhase, setCurrentPhase] = useState(2)
+    const [SelectedPhase, setSelectedPhase] = useState(CurrentPhase)
+    const handleSetSelectedPhase = (Id: number) => (SelectedPhase === Id ? setSelectedPhase(0) : setSelectedPhase(Id))
 
    return (
         <div className="flex flex-col items-center">
@@ -29,17 +27,23 @@ const Roadmap: FC<RoadmapProps> = ({ phaseSummaries, phaseDescriptions}) => {
                         <h3 className="pb-4 text-lg font-semibold">Phase 1</h3>
                         <div className="flex items-center">
                             <div className="hidden sm:flex w-full bg-white h-0"></div>
-                            <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
+                            {CurrentPhase === 0 ? (
+                              <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                                <BsLightningFill color="#A2C11C" fontSize="1.5em" />
+                                </div>  
+                            ) : (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
                                 <BsLightningFill color="white" fontSize="1.5em" />
-                            </div>
+                                </div>
+                            )}                           
                             <div className="hidden sm:flex w-full bg-white h-1"></div>
                         </div>
                         <div className="mt-3 mx-4 sm:pr-8">
                             <p className="text-sm font-normal">{phaseSummaries[0]}</p>
-                        </div>
+                        </div> 
                         <div className="flex justify-center pt-6">
-                            <button onClick={() => handleSetSelectedPhase(Index)} >
-                                <SlArrowDown color="white" fontSize="2em"/>
+                            <button onClick={() => handleSetSelectedPhase(0)} >
+                                {SelectedPhase === 0 ? (<SlArrowDown color="#053a42" fontSize="2em"/>) : (<SlArrowDown color="white" fontSize="2em"/>)}                       
                             </button>
                         </div>
                     </li>
@@ -47,17 +51,23 @@ const Roadmap: FC<RoadmapProps> = ({ phaseSummaries, phaseDescriptions}) => {
                         <h3 className="pb-4 text-lg font-semibold">Phase 2</h3>
                         <div className="flex items-center">
                             <div className="hidden sm:flex w-full bg-white h-1"></div>
-                            <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
+                            {CurrentPhase === 1 ? (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                                <TbLayersLinked color="#A2C11C" fontSize="2em" />
+                            </div>
+                            ) : (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
                                 <TbLayersLinked color="white" fontSize="2em" />
                             </div>
+                            )}
                             <div className="hidden sm:flex w-full bg-white h-1"></div>
                         </div>
                         <div className="mt-3 mx-4 sm:pr-8">
                             <p className="text-sm font-normal">{phaseSummaries[1]}</p>
                         </div>
                         <div className="flex justify-center pt-6">
-                            <button onClick={() => handleSetSelectedPhase(Index)} >
-                                <SlArrowDown color="white" fontSize="2em"/>
+                            <button onClick={() => handleSetSelectedPhase(1)} >
+                                {SelectedPhase === 1 ? (<SlArrowDown color="#053a42" fontSize="2em"/>) : (<SlArrowDown color="white" fontSize="2em"/>)}
                             </button>
                         </div>
                     </li>
@@ -65,17 +75,23 @@ const Roadmap: FC<RoadmapProps> = ({ phaseSummaries, phaseDescriptions}) => {
                         <h3 className="pb-4 text-lg font-semibold">Phase 3</h3>
                         <div className="flex items-center">
                             <div className="hidden sm:flex w-full bg-white h-1"></div>
-                            <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                            {CurrentPhase === 2 ? (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
                                 <BsGearWideConnected color="#A2C11C" fontSize="1.5em" />
                             </div>
+                            ) : (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
+                                <BsGearWideConnected color="white" fontSize="1.5em" />
+                            </div>
+                            )}                    
                             <div className="hidden sm:flex w-full bg-white h-1"></div>
                         </div>
                         <div className="mt-3 mx-4 sm:pr-8">
                             <p className="text-sm font-normal">{phaseSummaries[2]}</p>
                         </div>
                         <div className="flex justify-center pt-6">
-                            <button onClick={() => handleSetSelectedPhase(Index)} >
-                                <SlArrowDown color="white" fontSize="2em"/>
+                            <button onClick={() => handleSetSelectedPhase(2)} >
+                                {SelectedPhase === 2 ? (<SlArrowDown color="#053a42" fontSize="2em"/>) : (<SlArrowDown color="white" fontSize="2em"/>)}
                             </button>
                         </div>
                     </li>
@@ -83,17 +99,23 @@ const Roadmap: FC<RoadmapProps> = ({ phaseSummaries, phaseDescriptions}) => {
                         <h3 className="pb-4 text-lg font-semibold">Phase 4</h3>
                         <div className="flex items-center">
                             <div className="hidden sm:flex w-full bg-white h-1"></div>
-                            <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
-                                <GiBugleCall color="white" fontSize="1.5em" />
-                            </div>
+                            {CurrentPhase === 3 ? (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                                    <GiBugleCall color="#A2C11C" fontSize="1.5em" />
+                                </div>
+                            ) : (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
+                                    <GiBugleCall color="white" fontSize="1.5em" />
+                                </div>
+                            )}       
                             <div className="hidden sm:flex w-full bg-white h-1"></div>
                         </div>
                         <div className="mt-3 mx-4 sm:pr-8">
                             <p className="text-sm font-normal">{phaseSummaries[3]}</p>
                         </div>
                         <div className="flex justify-center pt-6">
-                            <button onClick={() => handleSetSelectedPhase(Index)} >
-                                <SlArrowDown color="white" fontSize="2em"/>
+                            <button onClick={() => handleSetSelectedPhase(3)} >
+                                {SelectedPhase === 3 ? (<SlArrowDown color="#053a42" fontSize="2em"/>) : (<SlArrowDown color="white" fontSize="2em"/>)}
                             </button>
                         </div>
                     </li>
@@ -101,17 +123,23 @@ const Roadmap: FC<RoadmapProps> = ({ phaseSummaries, phaseDescriptions}) => {
                         <h3 className="pb-4 text-lg font-semibold">Phase 5</h3>
                         <div className="flex items-center">
                             <div className="hidden sm:flex w-full bg-white h-1"></div>
-                            <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
-                                <FaPaperPlane color="white" fontSize="1.5em" />
-                            </div>
+                            {CurrentPhase === 4 ? (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                                    <FaPaperPlane color="#A2C11C" fontSize="1.5em" />
+                                </div>
+                            ) : (
+                                <div className="flex z-10 justify-center items-center w-10 h-10 rounded-full ring-0 ring-brand-dark sm:ring-8 shrink-0">
+                                    <FaPaperPlane color="white" fontSize="1.5em" />
+                                </div>
+                            )}                     
                             <div className="hidden sm:flex w-full bg-white h-0"></div>
                         </div>
                         <div className="mt-3 mx-4 sm:pr-8">
                             <p className="text-sm font-normal">{phaseSummaries[4]}</p>
                         </div>
                         <div className="flex justify-center pt-6">
-                            <button onClick={() => handleSetSelectedPhase(Index)} >
-                                <SlArrowDown color="white" fontSize="2em"/>
+                            <button onClick={() => handleSetSelectedPhase(4)} >
+                                {SelectedPhase === 4 ? (<SlArrowDown color="#053a42" fontSize="2em"/>) : (<SlArrowDown color="white" fontSize="2em"/>)}
                             </button>
                         </div>
                     </li>                 
