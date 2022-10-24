7ebd7cf37ab9f2f9a7d3de9dc07632aa23f3e707
Falco Rodenburg <falcorodenburg@gmail.com>
2022-10-24 10:19:29 +0200

    store types in separate folder

---
+++ Types/Project.ts
@@ -0,0 +1,15 @@
+export type Project = {
+    id: number,
+    name: string,
+    description: string,
+    network: string,
+    category1: string,
+    category2: string,
+    repository: string,
+    image: string,
+    active: boolean,
+    loginType: string,
+    login: string,
+    phaseSummaries: string[],
+    phaseDescriptions: string[][]
+}
