39c14551167a3916af75fc250ea876ff62d21745
Rich Warner <richwarneriii@gmail.com>
2022-10-27 10:36:12 -0600

    Implemented buttons on project page

---
+++ config/goerli-alpha.tsx
@@ -0,0 +1,3 @@
+export const network = "goerli-alpha"
+export const kickStarkAddr = "0x0160f2675609eea0bfed76ea9a509b939edeb09f9b104b05235f13611fb63e9d"
+export const validatorAddr = "0x037329aa67100c5d855bd00230d77801b12ca36e05ac624ac7f9d7b4ca984873"
