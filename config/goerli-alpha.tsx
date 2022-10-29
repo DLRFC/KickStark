852db8f22bf8a8cd527e8eab8dd68a51464040ee
Rich Warner <richwarneriii@gmail.com>
2022-10-29 16:44:44 -0600

    Updated contract interaction buttons

--- config/goerli-alpha.tsx
+++ config/goerli-alpha.tsx
@@ -1,3 +1,3 @@
 export const network = "goerli-alpha"
-export const kickStarkAddr = "0x0160f2675609eea0bfed76ea9a509b939edeb09f9b104b05235f13611fb63e9d"
+export const kickStarkAddr = "0x01cda30c60732934d3032aad0268a0d553ef355396ec7b0c798a64c5c470bd32"
 export const validatorAddr = "0x037329aa67100c5d855bd00230d77801b12ca36e05ac624ac7f9d7b4ca984873"
--- pages/catalogue/[projectID].tsx
+++ pages/catalogue/[projectID].tsx
@@ -12,7 +12,7 @@ import { gql } from "@apollo/client"
 import { Project } from "../../Types/Project"
 import { AppContext } from "../../components/context/AppContext"
 import kickStarkAbi from "../../abis/kickStarkAbi.json"
-import { kickStarkAddr } from "../../config/goerli-alpha"
+import { kickStarkAddr, validatorAddr } from "../../config/goerli-alpha"
 
 type Props = {
     project: Project
@@ -20,20 +20,59 @@ type Props = {
 }
 
 const ProjectProfile: NextPage<Props> = ({ project, githubMetrics }) => {
-    const [projectIsClosed, setProjectIsClosed] = useState<boolean>(true)
+    const [projectIsOpen, setProjectIsOpen] = useState<boolean>(false)
     const { userAddress, setAppContext } = useContext(AppContext)
 
     useEffect(() => {
-        setProjectStatus()
+        fetchProjectIsOpen()
     }, [])
 
-    const setProjectStatus = async () => {
+    const fetchProjectIsOpen = async () => {
         const starknet = getStarknet()
         const kickStarkContract = new Contract(kickStarkAbi as Abi, kickStarkAddr, starknet.provider)
-        const isClosed = await kickStarkContract.isClosed()
-        setProjectIsClosed(isClosed)
-        console.log("Project closed?", isClosed == true, isClosed, isClosed[0].negative == true)
+        const isOpen = await kickStarkContract.isOpen()
+        const convertedIsOpen = isOpen == true
+        setProjectIsOpen(convertedIsOpen)
+        console.log("Project open?", convertedIsOpen)
     }
+
+    // Validator functions
+
+    const startProject = async () => {
+        const wallet = getStarknet()
+        if (wallet.isConnected) {
+            const kickStarkContract = new Contract(kickStarkAbi as Abi, kickStarkAddr, wallet.account)
+            return kickStarkContract.start_project(validatorAddr)
+        }
+    }
+
+    const nextStage = async () => {
+        const wallet = getStarknet()
+        if (wallet.isConnected) {
+            const kickStarkContract = new Contract(kickStarkAbi as Abi, kickStarkAddr, wallet.account)
+            return kickStarkContract.nextStage()
+        }
+    }
+
+    const closeProject = async () => {
+        const wallet = getStarknet()
+        if (wallet.isConnected) {
+            const kickStarkContract = new Contract(kickStarkAbi as Abi, kickStarkAddr, wallet.account)
+            return kickStarkContract.closeProject()
+        }
+    }
+
+    // Builder/owner functions
+
+    const claim = async () => {
+        const wallet = getStarknet()
+        if (wallet.isConnected) {
+            const kickStarkContract = new Contract(kickStarkAbi as Abi, kickStarkAddr, wallet.account)
+            return kickStarkContract.project_claim()
+        }
+    }
+
+    // Contributor functions
 
     const contribute = async (amount: number) => {
         const wallet = getStarknet()
@@ -41,14 +80,6 @@ const ProjectProfile: NextPage<Props> = ({ project, githubMetrics }) => {
             const kickStarkContract = new Contract(kickStarkAbi as Abi, kickStarkAddr, wallet.account)
             // Project needs to be started first for this to work
             return kickStarkContract.contribute(amount)
-        }
-    }
-
-    const claim = async () => {
-        const wallet = getStarknet()
-        if (wallet.isConnected) {
-            const kickStarkContract = new Contract(kickStarkAbi as Abi, kickStarkAddr, wallet.account)
-            return kickStarkContract.project_claim()
         }
     }
 
@@ -88,46 +119,90 @@ const ProjectProfile: NextPage<Props> = ({ project, githubMetrics }) => {
 
             <div className="flex mx-[10%] my-[5%]">
                 <div className=" w-100% border-8 border-brand-orange rounded-lg mr-8 p-12">
-                    <div className="my-10 text-brand-green text-2xl font-bold text-center">Invest</div>
+                    {userAddress === null ? (
+                        <div className="my-10 text-brand-green text-2xl font-bold text-center">
+                            Connect wallet to perform actions
+                        </div>
+                    ) : (
+                        <>
+                            <div className="my-10 text-brand-green text-2xl font-bold text-center">
+                                Contributor Actions
+                            </div>
 
-                    <div className="justify-between p-8">
-                        {!projectIsClosed && (
-                            <div className="flex flex-col pb-6">
-                                {userAddress !== null && <input className="py-2 rounded-md"></input>}
+                            <div className="flex flex-col mb-4">
+                                <input id="deposit-amt" className="py-2 rounded-md" />
                                 <button
-                                    onClick={(e) => contribute(parseFloat((e.target as HTMLInputElement).value))}
+                                    onClick={(e) =>
+                                        contribute(
+                                            parseFloat(
+                                                (document.querySelector("#deposit-amt") as HTMLInputElement).value
+                                            )
+                                        )
+                                    }
                                     className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
-                                    disabled={userAddress === null}
+                                    disabled={!projectIsOpen}
                                 >
-                                    {userAddress === null && "Connect to "}Deposit Funds
+                                    Deposit Funds
                                 </button>
                             </div>
-                        )}
-                        {userAddress !== null && (
-                            <>
-                                <div className="flex flex-col pb-6">
-                                    {/* <input className="py-2 rounded-md"></input> */}
-                                    <button
-                                        onClick={claim}
-                                        className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
-                                    >
-                                        Builder Claim
-                                    </button>
-                                </div>
-                                {projectIsClosed && (
-                                    <div className="flex flex-col">
-                                        {/* <input className="py-2 rounded-md"></input> */}
-                                        <button
-                                            onClick={withdraw}
-                                            className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
-                                        >
-                                            Investor Refund
-                                        </button>
-                                    </div>
-                                )}
-                            </>
-                        )}
-                    </div>
+
+                            <div className="flex flex-col">
+                                <button
+                                    onClick={withdraw}
+                                    className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
+                                    disabled={projectIsOpen}
+                                >
+                                    Investor Refund
+                                </button>
+                            </div>
+
+                            <div className="my-10 text-brand-green text-2xl font-bold text-center">Builder Actions</div>
+
+                            <div className="flex flex-col mb-4">
+                                <button
+                                    onClick={claim}
+                                    className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
+                                    disabled={!projectIsOpen}
+                                >
+                                    Claim Funds
+                                </button>
+                            </div>
+
+                            <div className="my-10 text-brand-green text-2xl font-bold text-center">
+                                Validator Actions
+                            </div>
+
+                            <div className="flex flex-col mb-4">
+                                <button
+                                    onClick={startProject}
+                                    className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
+                                    disabled={projectIsOpen}
+                                >
+                                    Start Project
+                                </button>
+                            </div>
+
+                            <div className="flex flex-col mb-4">
+                                <button
+                                    onClick={nextStage}
+                                    className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
+                                    disabled={!projectIsOpen}
+                                >
+                                    Complete Stage
+                                </button>
+                            </div>
+
+                            <div className="flex flex-col mb-4">
+                                <button
+                                    onClick={closeProject}
+                                    className="my-3 bg-brand-orange text-brand-darkest rounded-lg py-2 px-4"
+                                    disabled={!projectIsOpen}
+                                >
+                                    Close Project
+                                </button>
+                            </div>
+                        </>
+                    )}
                 </div>
 
                 <div className="items-center flex justify-center">
@@ -189,7 +264,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
                 mergedPullRequests: mergedPullRequests,
                 commits: commits
             }
-
         }
     } catch (error) {
         console.log("Error", error)
